faadb0ac2f93992a6027147f781277eed747dd30
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-17 00:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
-document.querySelector('main').remove();
+document.querySelector('#main').remove();
 
 /* Create your new element here and assign it to newHeader */
-
+const newHeader = null;
 let newHeader = document.createElement('h1');
 newHeader.innerText = "Jesse is the champion!";
 let div = document.querySelector('#victory');
