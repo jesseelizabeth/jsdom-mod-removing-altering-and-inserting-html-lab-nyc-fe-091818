4f876182d6d89d28837ac15d5b60a7f013659624
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-17 00:32:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
+document.querySelector('#main').remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const header = document.createElement('h1');
+header.id = 'victory'
+header.innerText = "Jesse is the champion!";
+document.body.appendChild(header);
+const newHeader = document.querySelector('#victory');
